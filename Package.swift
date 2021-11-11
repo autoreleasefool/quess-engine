f7391dd50e8a0e1201983c6f06e8db50d113030d
Joseph Roque <contact@josephroque.ca>
2021-11-10 23:11:21 -0800

    Initial project commit

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "QuessEngine",
+    platforms: [
+      .iOS(.v13),
+      .macOS(.v10_15),
+      .tvOS(.v13),
+      .watchOS(.v6),
+    ],
+    products: [
+        .library(
+            name: "QuessEngine",
+            targets: ["QuessEngine"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "QuessEngine",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "QuessEngineTests",
+            dependencies: ["QuessEngine"]
+        ),
+    ]
+)
---
+++ Sources/QuessEngine/QuessEngine.swift
@@ -0,0 +1,6 @@
+public struct QuessEngine {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
