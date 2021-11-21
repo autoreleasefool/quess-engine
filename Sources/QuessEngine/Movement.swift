9d33c6c0e9ad1343a7547921bf1d20f1c67b30fd
Joseph Roque <contact@josephroque.ca>
2021-11-20 17:54:11 -0800

    Quessify and Chessify

--- Sources/QuessEngine/Movement.swift
+++ Sources/QuessEngine/Movement.swift
@@ -14,6 +14,14 @@ public struct Movement: Hashable {
     self.to = to
   }
 
+  public func quessify(withState state: GameState) throws -> QuessMove {
+    guard let from = state.board.position(ofPiece: piece) else {
+      throw QuessEngineError.failedConversion
+    }
+
+    return QuessMove(from: from, to: to)
+  }
+
 }
 
 public struct QuessMove: Hashable {
@@ -24,6 +32,34 @@ public struct QuessMove: Hashable {
   public init(from: Board.RankFile, to: Board.RankFile) {
     self.from = from
     self.to = to
+  }
+
+  public func chessify(withState state: GameState) throws -> Movement {
+    guard let piece = state.board.pieceAt(from) else {
+      throw QuessEngineError.failedConversion
+    }
+
+    return Movement(piece: piece, to: to)
+  }
+
+}
+
+public enum QuessEngineError: Error {
+  case failedConversion
+}
+
+public extension Collection where Element == QuessMove {
+
+  func chessify(withState state: GameState) throws -> [Movement] {
+    try map { try $0.chessify(withState: state) }
+  }
+
+}
+
+public extension Collection where Element == Movement {
+
+  func quessify(withState state: GameState) throws -> [QuessMove] {
+    try map { try $0.quessify(withState: state) }
   }
 
 }
